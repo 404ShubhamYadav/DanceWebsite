4d65a4065472ef86caac01db656807257499a860
Shubham Kr. Yadav <shubhamyadav5594@gmail.com>
2022-05-07 00:55:58 +0530

    Fixed th bugs

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser"); /*To save data of request body in DB using post request with the help of express*/
+mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true});
 const port = 8000;
+
+// Define mongoose schema
+var contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+  });
+
+var Contact = mongoose.model('Contact', contactSchema);
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
@@ -19,6 +33,15 @@ app.get('/', (req, res)=>{
 app.get('/contact', (req, res)=>{
     const params = { }
     res.status(200).render('contact.pug', params);
+})
+app.post('/contact', (req, res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.send("This item has been saved in database")
+    }).catch(()=>{
+        res.status(400).send("Item was not saved to the database")
+    })
+    // res.status(200).render('contact.pug', params);
 })
 
 // START THE SERVER
